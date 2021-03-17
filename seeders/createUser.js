ab937f286ea6673e9badcf14a1ff267371e92aae
glamboyosa <ogbemudiatimothy@gmail.com>
2021-03-17 14:38:05 +0100

    feat: starter-files

---
+++ seeders/createUser.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+module.exports = {
+  up: (queryInterface) =>
+    queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "johndoe",
+          password: db.User.generateHash("password"),
+          verificationMethod: "sms",
+          phoneNumber: "+447743868561",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    ),
+  down: (queryInterface) => queryInterface.bulkDelete("Users", null, {}),
+};
